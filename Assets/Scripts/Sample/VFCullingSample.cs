bcada43f7a3206ac8f1380f8c60c6272d0007f6e
MPozek <mislav.pozek@fer.hr>
2019-03-02 14:28:58 +0100

    first iteration of VF culling, adds a sample as well

---
+++ Assets/Scripts/Sample/VFCullingSample.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class VFCullingSample : MonoBehaviour
+{
+    public int NumObjects = 10000;
+
+    public bool DrawOnlyCulled;
+
+    public Camera TargetCamera;
+
+    private NativeArray<float3> _positions;
+    private NativeArray<float3> _extents;
+
+    private NativeList<int> _filteredIndices;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _positions = new NativeArray<float3>(NumObjects, Allocator.Persistent);
+        _extents = new NativeArray<float3>(NumObjects, Allocator.Persistent);
+
+        _filteredIndices = new NativeList<int>(NumObjects, Allocator.Persistent);
+
+        Unity.Mathematics.Random rand = new Unity.Mathematics.Random(123);
+
+        for (int i = 0; i < NumObjects; i++)
+        {
+            _positions[i] = rand.NextFloat3Direction() * rand.NextFloat(1f, 50f);
+            _extents[i] = new float3(0.5f, 0.5f, 0.5f);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        _positions.Dispose();
+        _extents.Dispose();
+        _filteredIndices.Dispose();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        _filteredIndices.Clear();
+
+        VFCulling.ScheduleCullingJob(TargetCamera.cullingMatrix, _positions, _extents, _filteredIndices).Complete();
+
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (Application.isPlaying)
+        {
+            if (DrawOnlyCulled)
+            {
+                for (int i = 0; i < _filteredIndices.Length; i++)
+                {
+                    int idx = _filteredIndices[i];
+                    Gizmos.DrawWireSphere(_positions[idx], 1f);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < NumObjects; i++)
+                {
+                    Gizmos.DrawWireSphere(_positions[i], 1f);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/VFCulling.cs
@@ -0,0 +1,55 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
+using UnityEngine;
+
+public static class VFCulling
+{
+    [BurstCompile]
+    private struct FilterViewFrustrumCulling : IJobParallelForFilter
+    {
+        [ReadOnly, DeallocateOnJobCompletion] public NativeArray<float4> FrustrumPlanes;
+        [ReadOnly] public NativeArray<float3> Positions;
+        [ReadOnly] public NativeArray<float3> Extents;
+
+        public bool Execute(int i)
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                float3 planeNormal = FrustrumPlanes[j].xyz;
+                
+                float planeConstant = FrustrumPlanes[j].w;
+
+                if (
+                    math.dot(Extents[i], math.abs(planeNormal)) + 
+                    math.dot(planeNormal, Positions[i]) + 
+                    planeConstant <= 0f)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+
+    private static readonly Plane[] _planes = new Plane[6];
+
+    public static JobHandle ScheduleCullingJob(float4x4 worldProjectionMatrix, NativeArray<float3> positions, NativeArray<float3> extents, NativeList<int> outIndices)
+    {
+        NativeArray<float4> frustrumPlanes = new NativeArray<float4>(6, Allocator.TempJob);
+        
+        GeometryUtility.CalculateFrustumPlanes(worldProjectionMatrix, _planes);
+
+        for (int i = 0; i < 6; ++i)
+        {
+            frustrumPlanes[i] = new float4(_planes[i].normal, _planes[i].distance);
+        }
+
+        return new FilterViewFrustrumCulling
+        {
+            FrustrumPlanes = frustrumPlanes,
+            Positions = positions,
+            Extents = extents
+        }.ScheduleAppend(outIndices, positions.Length, 16);
+    }
+}
